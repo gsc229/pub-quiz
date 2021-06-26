09b91eb48bc52c83a293faa0135b59fa2ceb5f1f
greg <gsc229@gmail.com>
2021-06-25 20:48:37 -0400

    quiz working

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import React, { Component, ReactElement } from 'react'
+// Types 
+import { AnswerObject } from '../App'
 
-const QuestionCard = () => {
-  return (
-    <div>
-      Question Card
-    </div>
-  )
+type Props = {
+  question: string
+  answers: string[]
+  callback: (e:React.MouseEvent<HTMLButtonElement>) => void
+  userAnswer: AnswerObject | undefined
+  questionNr: number
+  totalQuestions: number
 }
+
+
+const QuestionCard = ({ question, answers, callback, userAnswer, questionNr, totalQuestions }: Props) => (
+    <div>
+      <p className="number">
+        Question: { questionNr } / { totalQuestions }
+      </p>
+      <p dangerouslySetInnerHTML={{ __html: question }}/>
+      <div>
+        {answers.map(answer =>(
+          <div key={answer}>
+            <button disabled={userAnswer !== undefined} value={answer}  onClick={callback}>
+              <span dangerouslySetInnerHTML={{ __html: answer}} />
+            </button>
+          </div>
+        ))}
+      </div>
+    </div>
+)
+
 
 export default QuestionCard
