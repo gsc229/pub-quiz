354bf519a567aa6f6b992a90ff5ffca49dfd3bf3
greg <gsc229@gmail.com>
2021-06-26 15:35:44 -0400

    added two api functions, one for session token, one for fetching categories. Added

--- src/API.ts
+++ src/API.ts
@@ -17,15 +17,59 @@ export enum Difficulty {
   HARD = "hard"
 }
 
-export const fetchQuizQuestions = async(amount: number, difficulty: Difficulty) => {
-  const endpoint = `https://opentdb.com/api.php?amount=${amount}&type=multiple&difficulty=${difficulty}`
-  const data = await (await fetch(endpoint)).json()
-  console.log({data})
+type DataReturn = {
+  success: boolean,
+  data?: object
+}
 
-  return data.results.map((question: Question) => (
-    {
-      ...question,
-      choices: shuffleArray([...question.incorrect_answers, question.correct_answer])
-    }
-  ))
+export const fetchQuizQuestions = async(amount: number, difficulty: Difficulty):Promise<DataReturn> => {
+
+  const endpoint = `https://opentdb.com/api.php?amount=${amount}&type=multiple&difficulty=${difficulty}`
+
+  const data = await (await fetch(endpoint)).json()
+
+  if (data){
+
+    const shuffledData = data.results.map((question: Question) => (
+      {
+        ...question,
+        choices: shuffleArray([...question.incorrect_answers, question.correct_answer])
+      }
+    ))
+
+    return { success: true, data: shuffledData }
+
+  }
+
+  return { success: false }
+  
+}
+
+
+export const fetchSessionToken = async(): Promise<DataReturn> => {
+
+  const endpoint = 'https://opentdb.com/api_token.php?command=request'
+
+  const data: { response_code: number, response_message: string, token: string } = await (await fetch(endpoint)).json()
+
+  localStorage.setItem('token', data.token)
+
+  if(data && data.token) return { success: true, data }
+
+  return { success: false }
+
+}
+
+export const fetchCategories = async(): Promise<DataReturn> => {
+
+  const endpoint = 'https://opentdb.com/api_category.php?command=request'
+
+  const data: { trivia_categories: [] } = await (await fetch(endpoint)).json()
+
+  localStorage.setItem('trivia_categories', JSON.stringify(data.trivia_categories))
+
+  if(data && data.trivia_categories) return { success: true, data }
+
+  return { success: false }
+
 }
